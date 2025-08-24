771ca3c8d62f18db605020c7be8c41f85ea68fdd
Subroto Kumar <subrotokumar021@gmail.com>
2025-08-24 18:41:38 +0600

    add active route

--- home.js
+++ home.js
@@ -33,6 +33,19 @@ function toggleService(id){
   document.getElementById(id).style.display = "block";
 }
 
+// active status reusable code
+function activeStatus(id){
+  const activeStatusClass = document.getElementsByClassName("active-status");
+
+  for(let i of activeStatusClass){
+    i.classList.remove("border-[#0874f2]", "bg-[#0874f20d]");
+    i.classList.add('border-gray-300');
+  }
+
+  document.getElementById(id).classList.remove('border-gray-300');
+  document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]");
+}
+
 // user pin number
 const userPin = 1234;
 
@@ -119,25 +132,49 @@ document.getElementById("logout-btn").addEventListener('click', function(){
 
 // section toggle 
 document.getElementById("add-money-area").addEventListener('click', function(){
+  // toggle form
   toggleService("add-money-section");
+
+  // active style
+  activeStatus('add-money-area');
 });
 
 document.getElementById("cashout-area").addEventListener("click", function () {
+  // toggle form
   toggleService("cashout-section");
+
+  // active style
+  activeStatus('cashout-area');
 });
 
 document.getElementById("transfer-money-area").addEventListener("click", function () {
+  // toggle form
   toggleService("transfer-money-section");
+
+  // active style
+  activeStatus('transfer-money-area');
 });
 
 document.getElementById("get-bonus-area").addEventListener("click", function () {
+  // toggle form
   toggleService("get-bonus-section");
+
+  // active style
+  activeStatus("get-bonus-area");
 });
 
 document.getElementById("pay-bill-area").addEventListener("click", function () {
+  // toggle form
   toggleService("pay-bill-section");
+
+  // active style
+  activeStatus("pay-bill-area");
 });
 
 document.getElementById("transaction-area").addEventListener("click", function () {
+  // toggle form
   toggleService("transaction-section");
+
+  // active style
+  activeStatus('transaction-area');
 });
